2639b51f70246289477dd89e586b063ca8b4d04b
AnneKay17 <2833669@students.wits.ac.za>
2026-07-06 00:37:35 +0200

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Login from './pages/Auth/Login'
+
+function App() {
+
+  return (
+    <>
+      {/*Feel free to comment this out*/}
+      <Login/>
+    </>
+  )
+}
+
+export default App
